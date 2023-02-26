3c2c8d4f5cdebdcc97fc21d53cee7f0674be7541
SatomiKazeHiro <tonasgtl@gmail.com>
2023-02-26 22:27:39 +0800

    主页面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div class="bg"></div>
   </div>
 </template>
 
 <style lang="less" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    overflow: hidden;
+    &::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      background-image: url("./assets/bg.jpeg");
+      background-size: cover;
+      filter: blur(2px);
+      scale: 1.01;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import less from "less";
+Vue.use(less);
+
 new Vue({
   router,
   store,
