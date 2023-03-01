9bdad3ae333e36f11319f68678bbb85b44a53282
SatomiKazeHiro <tonasgtl@gmail.com>
2023-03-01 23:11:46 +0800

    首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
   <div id="app">
-    <div class="bg"></div>
+    <task-bar />
+    <desk-top />
+    <dock-bar />
   </div>
 </template>
+
+<script>
+import cmps from "./components";
+
+export default {
+  components: { ...cmps },
+};
+</script>
 
 <style lang="less" scoped>
 #app {
   width: 100%;
   height: 100%;
   position: relative;
-  .bg {
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  &::after {
+    content: "";
     position: absolute;
-    top: 0;
     left: 0;
-    width: 100%;
+    top: 0;
     height: 100%;
+    width: 100%;
+    background-image: url("./assets/bg.jpeg");
+    background-size: cover;
+    filter: blur(2px);
+    scale: 1.01;
     z-index: -1;
-    overflow: hidden;
-    &::after {
-      content: "";
-      position: absolute;
-      left: 0;
-      top: 0;
-      height: 100%;
-      width: 100%;
-      background-image: url("./assets/bg.jpeg");
-      background-size: cover;
-      filter: blur(2px);
-      scale: 1.01;
-    }
   }
 }
 </style>
---
+++ src/components/task-bar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="task-bar-wrap"></div>
+</template>
+
+<script>
+export default {
+  name: "TaskBar",
+};
+</script>
+
+<style lang="less" scoped>
+#task-bar-wrap {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.64);
+  position: relative;
+}
+</style>
